6e970aec72f8fd81d50dc49bb2873685e25e3fa5
HP <vallenfajar@gmail.com>
2019-11-29 16:58:23 +0700

    Camera

---
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.camera
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_camera.setOnClickListener{
+            var i= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i,123)
+        }
+
+
+        btn_galery.setOnClickListener{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    requestPermissions(permissions,PERMISSION_CODE);
+                }
+                else{
+                    pickImageFromGalerry();
+                }
+            }
+            else{
+                pickImageFromGalerry();
+            }
+
+        }
+    }
+    private fun pickImageFromGalerry(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent,IMAGE_PICK_CODE)
+    }
+    companion object{
+        private val IMAGE_PICK_CODE = 1000;
+        private val PERMISSION_CODE = 1001;
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,permission:Array<out String>,grantResults:IntArray){
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if(grantResults.size > 0 && grantResults [0]== PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGalerry()
+                }
+                else{
+                    Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int,resultCode: Int,data: Intent?){
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imageView.setImageURI(data?.data)
+        }
+            if(requestCode == 123)
+            {
+                var bmp= data?.extras?.get("data") as? Bitmap
+                imageView.setImageBitmap(bmp)
+            }
+        }
+    }
+
